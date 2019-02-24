7237fad470b5fa94735260eb177c5b2534731b09
FalconPilot <fpilot.github@gmail.com>
2019-02-24 14:37:28 +0100

    added F1 Fatma rifle

--- src/data/fsaf/index.js
+++ src/data/fsaf/index.js
@@ -1,6 +1,11 @@
+// Rifles
 import ALIB1 from './rifles/ali_b1'
+import F1FATMA from './rifles/f1_fatma'
+
+// Cannons
 import KAMEL9975 from './cannons/kamel_99-75'
 
+// Country object
 export default {
   name: 'Franco-Senegalese-Algerian Federation',
   acronym: 'FSAF',
@@ -15,7 +20,8 @@ export default {
     rifles: {
       label: 'Rifles',
       elements: {
-        ali_b1: ALIB1
+        ali_b1: ALIB1,
+        f1_fatma: F1FATMA
       }
     },
     cannons: {
---
+++ src/data/fsaf/rifles/f1_fatma.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import SVG from '../../../images/rifles/f1_fatma.svg'
+
+export default {
+  name: 'F1 Fatma',
+  image: <img src={SVG} />
+}
