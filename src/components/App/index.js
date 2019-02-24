ee8aa8462c1626745ba6ee47f1fe6cbcc9671f3a
FalconPilot <fpilot.github@gmail.com>
2019-02-24 17:18:03 +0100

    added OG tags and removed useless log

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -29,7 +29,7 @@ class App extends Component {
     this.setState({
       option: event.currentTarget.value,
       type: event.currentTarget.dataset.type
-    }, () => console.log(this.state))
+    })
   }
 
   changeCountry = (event) => {
