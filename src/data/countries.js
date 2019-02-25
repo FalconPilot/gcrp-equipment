8bbd7448b9bc5ae15b356bff65f65a17ab6fd128
Raphael Pallies <raphael@permettezmoideconstruire.fr>
2019-02-25 14:56:02 +0100

    added Regno d'Italia

--- src/data/countries.js
+++ src/data/countries.js
@@ -1,11 +1,13 @@
 import FSAF from './fsaf'
 import RPL from './rpl'
 import RR from './rr'
+import RI from './ri'
 import FSWP from './fswp'
 
 export default {
   FSAF,
   RPL,
   RR,
+  RI,
   FSWP
 }
