0f9a5f9b7532aaef2be64c7788e5b30287b00c4a
FalconPilot <fpilot.github@gmail.com>
2019-03-01 00:17:45 +0100

    added auth0 login/logout/signup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+import LoadingSpinner from '../LoadingSpinner'
+import Header from '../Header'
 import Menu from '../Menu'
 import Country from '../Country'
 import Weapon from '../Weapon'
 
+import { newAuth, checkSession, parseHash } from '../../utils/auth'
+
 import { version } from '../../../package.json'
-import data from '../../data/global'
 import countries from '../../data/countries'
 
 import './style.css'
@@ -14,6 +17,35 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = this.loadState()
+  }
+
+  componentWillMount() {
+    this.auth = newAuth()
+    parseHash(this.auth)
+      .then(res => res
+        ? Promise.resolve(res)
+        : Promise.reject(res)
+      )
+      .catch(() => checkSession(this.auth))
+      .then(res => {
+        console.log(res)
+        this.setState({
+          credentials: res
+        })
+      })
+      .catch(console.warn)
+      .then(() => {
+        this.setState({ loading: false })
+      })
+
+    // checkSession(this.auth)
+    //   .then(res => {
+    //     this.setState({ credentials: res })
+    //   })
+    //   .catch(console.warn)
+    //   .then(() => {
+    //     this.setState({ loading: false })
+    //   })
   }
 
   optionExist = (option, country_key) => {
@@ -45,7 +77,8 @@ class App extends Component {
       country_key: country[0],
       country: country[1],
       option: (optionExist && option) || 'COUNTRY',
-      type: (optionExist && type) || 'COUNTRY'
+      type: (optionExist && type) || 'COUNTRY',
+      loading: true
     }
   }
 
@@ -77,37 +110,49 @@ class App extends Component {
     WEAPON: Weapon
   }[type] || <div className='item-viewport' />)
 
+  logout = () => {
+    this.auth.logout({ returnTo: (window.location.origin + window.location.pathname).replace(/\/$/g, '') })
+  }
+
+  login = () => {
+    this.auth.authorize()
+  }
+
   render() {
     const Viewport = this.matchViewport(this.state.type)
-    return (
-      <React.Fragment>
-        <header className='app-header' style={{ backgroundColor: this.state.country.color }}>
-          <h1 style={{ color: this.state.country.title }}>{this.state.country.name}</h1>
-          <p style={{ color: this.state.country.title }} className='header-subtitle'>Year {data.year}</p>
-        </header>
-        <main className='app-container'>
-          <Menu
+    return this.state.loading
+      ? <LoadingSpinner />
+      : (
+        <React.Fragment>
+          <Header
+            credentials={this.state.credentials}
             country={this.state.country}
-            country_key={this.state.country_key}
-            option={this.state.option}
-            type={this.state.type}
-            updateValue={this.updateOption}
-            changeCountry={this.changeCountry}
+            login={this.login}
+            logout={this.logout}
           />
-          <Viewport
-            className='item-viewport'
-            option={this.state.option}
-            country={this.state.country}
-          />
-        </main>
-        <footer className='app-footer'>
-          <p>App v{version}</p>
-          <p>
-            Want to make Vectorial weapons ? Try <a href='https://vectr.com/' target='_blank'>Vectr</a> !
-          </p>
-        </footer>
-      </React.Fragment>
-    )
+          <main className='app-container'>
+            <Menu
+              country={this.state.country}
+              country_key={this.state.country_key}
+              option={this.state.option}
+              type={this.state.type}
+              updateValue={this.updateOption}
+              changeCountry={this.changeCountry}
+            />
+            <Viewport
+              className='item-viewport'
+              option={this.state.option}
+              country={this.state.country}
+            />
+          </main>
+          <footer className='app-footer'>
+            <p>App v{version}</p>
+            <p>
+              Want to make Vectorial weapons ? Try <a href='https://vectr.com/' target='_blank'>Vectr</a> !
+            </p>
+          </footer>
+        </React.Fragment>
+      )
   }
 }
 
